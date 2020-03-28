f611b9d39a72ed1727ec76c691d7350b143234b8
Anders Björkland <anders@femtearenan.se>
2020-03-28 16:42:06 +0100

    added form for uploading images

--- src/Form/PortfolioEntryType.php
+++ src/Form/PortfolioEntryType.php
@@ -2,16 +2,20 @@
 
 namespace App\Form;
 
-use App\Entity\Image;
+use App\Entity\PortfolioEntry;
+use App\Form\ImageType;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
-class ImageType extends AbstractType {
+class PortfolioEntryType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
+            ->add('name', TextType::class, ['label' => "Title"])
             ->add('image', FileType::class, [
                 'label' => 'Choose a file',
                 'mapped' => false,
@@ -26,13 +30,14 @@ class ImageType extends AbstractType {
                         ]
                     ])
                 ]
-            ]);
+            ])
+            ->add('textDescription', TextType::class, ['label' => "Description"]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Image::class,
+            'data_class' => PortfolioEntry::class,
         ]);
     }
 }
