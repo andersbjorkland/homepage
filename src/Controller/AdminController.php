0c2a1b9f3416879fa377d95b308563ce7033e150
Anders Björkland <anders@femtearenan.se>
2020-03-29 13:03:21 +0200

    added login and admin authentication

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Image;
+use App\Entity\PortfolioEntry;
+use App\Form\ImageType;
+use App\Form\PortfolioEntryType;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+use \RuntimeException;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="app_admin")
+     */
+    public function index()
+    {
+        return $this->render('admin.html.twig');
+    }
+}
