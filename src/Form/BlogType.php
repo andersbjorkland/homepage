1f9fbb7438b7f46e99c99c6fcdb5c39ff18e8a78
Anders Björkland <anders@femtearenan.se>
2020-03-31 19:37:32 +0200

    added feature to add multiple image files to the form

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -3,40 +3,31 @@
 namespace App\Form;
 
 use App\Entity\Post;
-use App\Form\ImageType;
+// use App\Form\ImageType;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
+// use Symfony\Component\Validator\Constraints\File;
 
 class BlogType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('title', TextType::class, ['label' => "Title"])
-            ->add('text', TextType::class, ['label' => "Text"])
+            ->add('text', TextareaType::class, ['label' => "Text"])
             ->add('images', CollectionType::class, [
                 'entry_type' => FileType::class,
                 'entry_options' => [
-                    
+                    'label' => false
                 ],
-                'mapped' => false,
-                'required' => false,
-                'constraints' => [
-                    new File([
-                        'maxSize' => '5M',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/jpg',
-                            'image/png'
-                        ]
-                    ])
-                        ],
-                'allow_add' => true,               
+                'allow_add' => true,
+                'by_reference' => false,         
             ])
             ->add('publishTime', DateTimeType::class, ['label' => "Publish"])
             ;
@@ -45,7 +36,7 @@ class BlogType extends AbstractType {
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Post::class,
+            'data_class' => File::class,
         ]);
     }
 }
