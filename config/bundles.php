63dab4316699786f34b6c74de4d81595b90e3ad2
Anders Björkland <git@andersbjorkland.online>
2020-09-20 15:32:45 +0200

    Updated to Symfony 5.1

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
